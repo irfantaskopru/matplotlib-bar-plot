dc08941e14027177daf393c5c7a28a1f4161b5bc
Irfan <79147749+irfantaskopru@users.noreply.github.com>
2022-11-04 00:17:00 +0300

    Update stacked-bars.py

--- stacked-bars.py
+++ stacked-bars.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 
 drinks = ["cappuccino", "latte", "chai", "americano", "mocha", "espresso"]
-sales1 =  [91, 76, 56, 66, 52, 27]
-sales2 = [65, 82, 36, 68, 38, 40]
+sales1 =  [91, 76, 56, 66, 52, 27] # B brand
+sales2 = [65, 82, 36, 68, 38, 40] # A brand
 
 plt.bar(range(len(sales2)), sales2)
 plt.bar(range(len(sales1)), sales1, bottom = sales2)
